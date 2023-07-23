d158de962468e859f5f754af75a286937a9c55e0
Dishant-jpggit config --global user.name Dishant-jpggit config --global user.email gupadishant90@gmail.comgit config --global user.name Dishant-jpg <84134946+Dishant-jpg@users.noreply.github.com>
2023-07-23 11:14:20 +0530

    Flutter

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/counter.dart';
 import 'package:flutter_application_1/simple.dart';
+import 'package:flutter_application_1/todo.dart';
 import 'package:flutter_application_1/twoNumber.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AddNumber(),
+      home: TodoListApp(),
     ),
   );
 }
